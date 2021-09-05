f8e970b3c8681bce9b441db7f4bf3052adfd7a60
TristanGavin <gavin.tristan@gmail.com>
2021-09-04 20:58:52 -0700

    add model evaluation and graphs

--- caruana/model_training.py
+++ caruana/model_training.py
@@ -2,18 +2,28 @@ import torch
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
-def train_model(model, dataloader, targets):
+def train_model(model, dataloaders, targets):
 
     # optimizer and loss function
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.3)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
     # loss_func = torch.nn.MSELoss()
     loss_func = torch.nn.BCELoss()  # Binary cross entropy - (label, probability of being 1)
-    max_iters = 500
+    max_iters = 5000
 
-    #train network
+    train_loader = dataloaders[0]
+    test_loader = dataloaders[1]
+
+    train_acc  = []
+    test_acc = []
+    stop = 0 # for early stopping criteria
+    max_acc = 0
+
+
+    # train network
     for epoch in range(max_iters):
-        for i, (inputs, y1, y2, y3, y4) in enumerate(dataloader):
+        for i, (inputs, y1, y2, y3, y4) in enumerate(train_loader):
             data = (inputs, y1, y2, y3, y4)
 
             # forward pass (batch gds)
@@ -33,22 +43,82 @@ def train_model(model, dataloader, targets):
             optimizer.zero_grad()   # clear gradients for next train
             loss.backward()         # backpropagation, compute gradients
             optimizer.step()        # take gradient step.
+
+
+        # evaluate model on train
+        model.eval()
+        with torch.no_grad():
+            for i, (inputs, y1, y2, y3, y4) in enumerate(train_loader):
+                data = (inputs, y1, y2, y3, y4)
+
+                # forward pass (batch gds)
+                y_pred = model(inputs)      
+                y_pred_cls = y_pred.round() # round all targets to 1 or 0
+                
+                # reshape data for multi targets
+                if len(targets) > 1:
+                    labels = [data[idx] for idx in targets]
+                    labels = torch.cat(labels, 1)
+                    # calculate acc
+                    tr_acc = y_pred_cls.eq(labels).sum() / (float(labels.shape[0]) * 2)
+                    train_acc.append(tr_acc)
+                else:
+                    labels = data[targets[0]]
+                    tr_acc = y_pred_cls.eq(labels).sum() / float(labels.shape[0]) 
+                    train_acc.append(tr_acc)
+
+
+        # evaluate model on test
+        model.eval()
+        with torch.no_grad():
+            for i, (inputs, y1, y2, y3, y4) in enumerate(test_loader):
+                data = (inputs, y1, y2, y3, y4)
+
+                y_pred = model(inputs)      
+                y_pred_cls = y_pred.round() # round all targets to 1 or 0
+
+                #TODO seperate accuracy for each target
+                # reshape data for multi targets
+                if len(targets) > 1:
+                    labels = [data[idx] for idx in targets]
+                    labels = torch.cat(labels, 1)
+                    te_acc = y_pred_cls.eq(labels).sum() / (float(labels.shape[0]) * 2)
+                    test_acc.append(te_acc)
+                else:
+                    labels = data[targets[0]]
+                    te_acc = y_pred_cls.eq(labels).sum() / float(labels.shape[0]) 
+                    test_acc.append(te_acc)
+
+        # early stopping criteria
+        if te_acc <= max_acc:
+            stop += 1
+        else:
+            max_acc = te_acc
+            stop = 0
+        
+        if (epoch > 500) and (stop == 500):
+            break
+           
         
     #TODO display the loss
-    # check model against test every so often or something.
-    return loss
+    plt.figure(figsize=(8, 8), dpi=80)
+    plt.plot(train_acc, label="train accuracy", color='dodgerblue', linewidth=1.5)
+    plt.plot(test_acc, label="test accuracy", color='red', linewidth=1.5)
+    plt.legend(frameon=False, prop={'size': 22})
+    plt.xlabel('Epochs')
+    plt.ylabel('% correct') 
+    plt.legend(frameon=False)
+    plt.tick_params(axis='both', which='major', labelsize=17)
+    plt.savefig('./graphs/loss_test.png')
+    plt.show()
+
+    return test_acc
 
 def test_model(model, X, labels):
-    num_targets = len(labels)
 
     loss_func = torch.nn.BCELoss()  # Binary cross entropy - (label, probability of being 1)
-
     accuracy = {}
-
     with torch.no_grad():
-
-        for target in range(num_targets):
-
             pred= model(X) # make prediction
 
             if num_targets > 1:
--- caruana/parity_nn.py
+++ caruana/parity_nn.py
@@ -3,6 +3,7 @@ from sklearn.utils import shuffle
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -54,23 +55,48 @@ class MultiTask(nn.Module):
         return x  
 
 ################################################################################################################################
-# SINGLE TASK MODELS
+# IMPORT DATA
 
 # format and shuffle data
 dataset = BinaryDataset()
-dataloader = DataLoader(dataset=dataset, batch_size=6, shuffle=True)
+dataset_size = len(dataset)
+valid_split = 0.2
+random_seed = 42
+
+# Creating data indices for training and validation splits:
+indices = list(range(dataset_size))
+split = int(np.floor(valid_split * dataset_size))
+np.random.seed(random_seed)
+np.random.shuffle(indices)
+train_indices, val_indices = indices[split:], indices[:split]
+
+# Creating PT data samplers and loaders:
+train_sampler = SubsetRandomSampler(train_indices)
+valid_sampler = SubsetRandomSampler(val_indices)
+
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=len(train_sampler), 
+                                           sampler=train_sampler)
+validation_loader = torch.utils.data.DataLoader(dataset, batch_size=len(valid_sampler),
+                                                sampler=valid_sampler)
+dataloaders = [train_loader, validation_loader]
 
 # initialize models
-single_task1 = MultiTask(1)
-# single_task2 = SingleTask()
-# single_task3 = SingleTask()
-# single_task4 = SingleTask()
+single_task1 = MultiTask(1) # task 1 
+single_task2 = MultiTask(1) # task 2
+single_task3 = MultiTask(1) # task 3 
+single_task4 = MultiTask(1) # task 4
        
-#train individual models
-# train_1loss = train_model(single_task1, dataloader, [1])
-# train_2loss = train_model(single_task2, X_train, [y2_train])
-# train_3loss = train_model(single_task3, X_train, y3_train)
-# train_4loss = train_model(single_task4, X_train, y4_train)
+# train individual models
+# train_1loss = train_model(single_task1, dataloaders, [1])
+# train_2loss = train_model(single_task2, dataloaders, [2])
+# train_3loss = train_model(single_task3, dataloaders, [3])
+# train_4loss = train_model(single_task4, dataloaders, [4])
+
+# save models torch.save(model.state_dict()
+# torch.save(single_task1.state_dict(), "./models/single_task1.pth") 
+# torch.save(single_task2.state_dict(), "./models/single_task2.pth") 
+# torch.save(single_task3.state_dict(), "./models/single_task3.pth") 
+# torch.save(single_task4.state_dict(), "./models/single_task4.pth") 
 
 ################################################################################################################################
 # MULTI-TASK MODELS
@@ -79,7 +105,7 @@ single_task1 = MultiTask(1)
 multitask1 = MultiTask(2)
 
 # train models
-train_loss = train_model(multitask1, dataloader, [1, 2])
+train_loss = train_model(multitask1, dataloaders, [1, 2])
 
 ################################################################################################################################
 # TEST MODEL
